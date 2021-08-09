d6f409b08af5d69a38555353e48b10dc32c548d0
Imran0561 <imran10uddin@gmail.com>
2021-08-09 13:09:44 +0530

    LockDownProject

---
+++ LockDownProject/src/org/test/cts/CourseDetails.java
@@ -0,0 +1,17 @@
+package org.test.cts;
+
+public class CourseDetails {
+	
+	
+	public static void hello() {
+		System.out.println("Hellow");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		System.out.println();
+		hello(); 
+		
+	}
+
+}
