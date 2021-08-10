44e252e7c9e8937b908dd955216f45cb11ccccf9
Imran0561 <imran10uddin@gmail.com>
2021-08-10 10:38:28 +0530

    Green address projects

---
+++ GreensAddress/src/org/add/GreensTech.java
@@ -0,0 +1,56 @@
+package org.add;
+
+public class GreensTech {
+	
+	public void greensOmr() {
+		System.out.println("Green OMR ADD : Balamurugan Garden, Okkiam,\n"
+				+ "Landmark: ICICI Bank Upstairs, Plot No.19,\n"
+				+ "Old Mahabalipuram Rd, Opposite to Sankeetha hotel, \n"
+				+ "Thoraipakkam, Tamil Nadu 600097");
+		System.out.println("---------------------------------------------");
+		
+	}
+	
+	public void greensAdayar() {
+		System.out.println("Green Adyar ADD : Greens technologys, No.11, 1st St,\n "
+				+ "Padmanabha Nagar, Adyar, Chennai, Tamil Nadu 600020");
+		System.out.println("---------------------------------------------");
+			
+		}
+	
+	public void greensTambaram() {
+		System.out.println("Greens Tambaram Add : ambaram Sanatorium, 1, Chennai -\n"
+				+ " Theni Hwy, near HP Petrol Bulk, Opposite Arulmigu \n"
+				+ "Sri Rama Anjaneyar Temple, Apparao Colony, Tambaram Sanatoruim,\n "
+				+ "Chennai, Tamil Nadu 600047");
+		System.out.println("---------------------------------------------");
+		
+	}
+	
+	public void greensVelacherry() {
+		System.out.println("Green Velacherry ADD: 28, Nagendra Nagar, opposite Phoenix \n"
+				+ " Market City, Velachery, Chennai, Tamil Nadu 600042");
+		System.out.println("---------------------------------------------");
+		
+	}
+	
+	public void greensAnnaNagar() {
+		System.out.println("Green Annanagar Add :  4th floor, SDV Arcade, AB-5, \n"
+				+ " 2nd Ave, AB Block, Shanthi Colony, Anna Nagar, Chennai,\n"
+				+ " Tamil Nadu 600040");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GreensTech gt = new GreensTech(); // object creation
+		gt.greensOmr();
+		gt.greensAdayar();
+		gt.greensTambaram();
+		gt.greensVelacherry();
+		gt.greensAnnaNagar();
+		
+		
+
+	}
+
+}
